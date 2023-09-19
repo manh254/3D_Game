a5eb05953f89b0c76add58266bc38bcec460b11d
Khuat Manh <khuatdinhmanh2001@gmail.com>
2023-09-19 14:56:04 +0700

    Triple Kill

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,29 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
     //public Sprite heroSprite;
-    public GameObject heroObject;
+    public GameObject[] heroObject;
+    private GameObject heroToBuyed;
 
     private BuildingSystem buildingSystem;
     private void Start()
     {
         buildingSystem = GameObject.Find("BuildingSystem").GetComponent<BuildingSystem>();
-        GetComponent<Button>().onClick.AddListener(BuyHero);
+        //GetComponent<Button>().onClick.AddListener(BuyHero);
     }
 
     private void BuyHero()
     {
-        buildingSystem.BuyHero(heroObject);
+        buildingSystem.BuyHero(heroToBuyed);
         Debug.Log("Buyed");
     }
 
     private void OnValidate()
     {
-        
+
+    }
+
+    public void BuyHero1()
+    {
+        heroToBuyed = heroObject[0];
+        BuyHero();
+    }
+    public void BuyHero2()
+    {
+        heroToBuyed = heroObject[1];
+        BuyHero();
+    }
+    public void BuyHero3()
+    {
+        heroToBuyed = heroObject[2];
+        BuyHero();
+    }
+    public void BuyHero4()
+    {
+        heroToBuyed = heroObject[3];
+        BuyHero();
+    }
+    public void BuyHero5()
+    {
+        heroToBuyed = heroObject[4];
+        BuyHero();
     }
 }
